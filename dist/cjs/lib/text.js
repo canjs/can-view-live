15190d90772d79ffc33a1f0d05c9723cf30e0761
Matthew Phillips <matthew@matthewphillips.info>
2016-06-27 12:36:50 -0400

    3.0.0-pre.3

---
+++ dist/cjs/lib/text.js
@@ -0,0 +1,21 @@
+/*can-view-live@3.0.0-pre.2#lib/text*/
+var live = require('./core.js');
+var nodeLists = require('can-view-nodelist');
+live.text = function (el, compute, parentNode, nodeList) {
+    var parent = live.getParentNode(el, parentNode);
+    var data = live.listen(parent, compute, function (ev, newVal, oldVal) {
+        if (typeof node.nodeValue !== 'unknown') {
+            node.nodeValue = live.makeString(newVal);
+        }
+        data.teardownCheck(node.parentNode);
+    });
+    var node = el.ownerDocument.createTextNode(live.makeString(compute()));
+    if (nodeList) {
+        nodeList.unregistered = data.teardownCheck;
+        data.nodeList = nodeList;
+        nodeLists.update(nodeList, [node]);
+        nodeLists.replace([el], node);
+    } else {
+        data.nodeList = live.replace([el], node, data.teardownCheck);
+    }
+};
