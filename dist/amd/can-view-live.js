8a3d6baeb2336a150af6faa64981550b25681543
Justin Meyer <justinbmeyer@gmail.com>
2016-07-28 15:52:29 -0500

    3.0.0-pre.4

---
+++ dist/amd/can-view-live.js
@@ -0,0 +1,10 @@
+/*can-view-live@3.0.0-pre.3#can-view-live*/
+define(function (require, exports, module) {
+    var live = require('./lib/core');
+    require('./lib/attr');
+    require('./lib/attrs');
+    require('./lib/html');
+    require('./lib/list');
+    require('./lib/text');
+    module.exports = live;
+});
