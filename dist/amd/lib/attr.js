051f6124411434a2c3b4cf0a7b2dc7441720a1b7
Justin Meyer <justinbmeyer@gmail.com>
2016-11-25 13:44:26 -0600

    3.0.2

---
+++ dist/amd/lib/attr.js
@@ -0,0 +1,11 @@
+/*can-view-live@3.0.1#lib/attr*/
+define(function (require, exports, module) {
+    var attr = require('can-util/dom/attr');
+    var live = require('./core');
+    live.attr = function (el, attributeName, compute) {
+        live.listen(el, compute, function (ev, newVal) {
+            attr.set(el, attributeName, newVal);
+        });
+        attr.set(el, attributeName, compute());
+    };
+});
