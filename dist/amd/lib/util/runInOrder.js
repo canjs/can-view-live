0b7b8d634b9378a093d71397bf54d33b16affba1
Justin Meyer <justinbmeyer@gmail.com>
2017-03-29 14:52:48 -0500

    3.1.0-pre.1

---
+++ dist/amd/lib/util/runInOrder.js
@@ -0,0 +1,28 @@
+/*can-view-live@3.1.0-pre.0#lib/util/runInOrder*/
+define(function (require, exports, module) {
+    module.exports = function makeRunInOrder() {
+        var running = 0;
+        var tasks = [];
+        return function runInOrder(fn) {
+            return function () {
+                var fnArgs = arguments;
+                if (running) {
+                    tasks.push({
+                        fn: fn,
+                        args: fnArgs
+                    });
+                    return;
+                }
+                running++;
+                fn.apply(null, fnArgs);
+                running--;
+                while (tasks.length) {
+                    running++;
+                    tasks[0].fn.apply(null, tasks[0].args);
+                    tasks.shift();
+                    running--;
+                }
+            };
+        };
+    };
+});
