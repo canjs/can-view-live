1ca5241bd0a14c1b98bb856a8370a89061a82bec
Kevin Phillips <kevin@bitovi.com>
2017-05-29 13:09:53 -0500

    3.1.0-pre.3

---
+++ dist/amd/lib/text.js
@@ -0,0 +1,24 @@
+/*can-view-live@3.1.0-pre.2#lib/text*/
+define(function (require, exports, module) {
+    var live = require('./core');
+    var nodeLists = require('can-view-nodelist');
+    var canReflect = require('can-reflect');
+    live.text = function (el, compute, parentNode, nodeList) {
+        var parent = live.getParentNode(el, parentNode);
+        var data = live.listen(parent, compute, function (newVal) {
+            if (typeof node.nodeValue !== 'unknown') {
+                node.nodeValue = live.makeString(newVal);
+            }
+            data.teardownCheck(node.parentNode);
+        });
+        var node = el.ownerDocument.createTextNode(live.makeString(canReflect.getValue(compute)));
+        if (nodeList) {
+            nodeList.unregistered = data.teardownCheck;
+            data.nodeList = nodeList;
+            nodeLists.update(nodeList, [node]);
+            nodeLists.replace([el], node);
+        } else {
+            data.nodeList = live.replace([el], node, data.teardownCheck);
+        }
+    };
+});
---
+++ dist/cjs/lib/attr.js
@@ -0,0 +1,10 @@
+/*can-view-live@3.1.0-pre.2#lib/attr*/
+var attr = require('can-util/dom/attr/attr');
+var live = require('./core.js');
+var canReflect = require('can-reflect');
+live.attr = function (el, attributeName, compute) {
+    live.listen(el, compute, function (newVal) {
+        attr.set(el, attributeName, newVal);
+    });
+    attr.set(el, attributeName, canReflect.getValue(compute));
+};
