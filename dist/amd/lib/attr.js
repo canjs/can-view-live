1933895ccd2a1da6254feba7fe66f082214fa663
Kevin Phillips <kevin@bitovi.com>
2017-06-22 22:35:39 -0500

    3.1.0

---
+++ dist/amd/lib/attr.js
@@ -0,0 +1,12 @@
+/*can-view-live@3.1.0-pre.8#lib/attr*/
+define(function (require, exports, module) {
+    var attr = require('can-util/dom/attr');
+    var live = require('./core');
+    var canReflect = require('can-reflect');
+    live.attr = function (el, attributeName, compute) {
+        live.listen(el, compute, function (newVal) {
+            attr.set(el, attributeName, newVal);
+        });
+        attr.set(el, attributeName, canReflect.getValue(compute));
+    };
+});
---
+++ dist/cjs/lib/util/runInOrder.js
@@ -0,0 +1,26 @@
+/*can-view-live@3.1.0-pre.8#lib/util/runInOrder*/
+module.exports = function makeRunInOrder() {
+    var running = 0;
+    var tasks = [];
+    return function runInOrder(fn) {
+        return function () {
+            var fnArgs = arguments;
+            if (running) {
+                tasks.push({
+                    fn: fn,
+                    args: fnArgs
+                });
+                return;
+            }
+            running++;
+            fn.apply(null, fnArgs);
+            running--;
+            while (tasks.length) {
+                running++;
+                tasks[0].fn.apply(null, tasks[0].args);
+                tasks.shift();
+                running--;
+            }
+        };
+    };
+};
