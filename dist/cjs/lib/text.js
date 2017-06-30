7c6ef5a301545ba3f3578915fab8a518fc601453
Justin Meyer <justinbmeyer@gmail.com>
2017-06-29 22:00:41 -0500

    3.2.0

---
+++ dist/cjs/lib/text.js
@@ -0,0 +1,21 @@
+/*can-view-live@3.2.0-pre.0#lib/text*/
+var live = require('./core.js');
+var nodeLists = require('can-view-nodelist');
+var canReflect = require('can-reflect');
+live.text = function (el, compute, parentNode, nodeList) {
+    var parent = live.getParentNode(el, parentNode);
+    var data = live.listen(parent, compute, function (newVal) {
+        if (typeof node.nodeValue !== 'unknown') {
+            node.nodeValue = live.makeString(newVal);
+        }
+    });
+    var node = el.ownerDocument.createTextNode(live.makeString(canReflect.getValue(compute)));
+    if (nodeList) {
+        nodeList.unregistered = data.teardownCheck;
+        data.nodeList = nodeList;
+        nodeLists.update(nodeList, [node]);
+        nodeLists.replace([el], node);
+    } else {
+        data.nodeList = live.replace([el], node, data.teardownCheck);
+    }
+};
