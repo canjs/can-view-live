2c87b6879696bef6d153135b9abbff43898d4fcd
imasustink <austin@austinkurpuis.com>
2017-11-01 14:43:50 -0700

    3.2.5

---
+++ dist/amd/lib/core.js
@@ -0,0 +1,101 @@
+/*can-view-live@3.2.4#lib/core*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-view-parser',
+    'can-util/dom/events',
+    'can-view-nodelist',
+    'can-util/dom/frag',
+    'can-util/dom/child-nodes',
+    'can-reflect',
+    'can-util/dom/events/removed'
+], function (require, exports, module) {
+    var parser = require('can-view-parser');
+    var domEvents = require('can-util/dom/events');
+    var nodeLists = require('can-view-nodelist');
+    var makeFrag = require('can-util/dom/frag');
+    var childNodes = require('can-util/dom/child-nodes');
+    var canReflect = require('can-reflect');
+    require('can-util/dom/events/removed');
+    var childMutationCallbacks = {};
+    var live = {
+        setup: function (el, bind, unbind) {
+            var tornDown = false, teardown = function () {
+                    if (!tornDown) {
+                        tornDown = true;
+                        unbind(data);
+                        domEvents.removeEventListener.call(el, 'removed', teardown);
+                    }
+                    return true;
+                }, data = {
+                    teardownCheck: function (parent) {
+                        return parent ? false : teardown();
+                    }
+                };
+            domEvents.addEventListener.call(el, 'removed', teardown);
+            bind(data);
+            return data;
+        },
+        listen: function (el, compute, change) {
+            return live.setup(el, function () {
+                canReflect.onValue(compute, change);
+            }, function (data) {
+                canReflect.offValue(compute, change);
+                if (data.nodeList) {
+                    nodeLists.unregister(data.nodeList);
+                }
+            });
+        },
+        getAttributeParts: function (newVal) {
+            var attrs = {}, attr;
+            parser.parseAttrs(newVal, {
+                attrStart: function (name) {
+                    attrs[name] = '';
+                    attr = name;
+                },
+                attrValue: function (value) {
+                    attrs[attr] += value;
+                },
+                attrEnd: function () {
+                }
+            });
+            return attrs;
+        },
+        isNode: function (obj) {
+            return obj && obj.nodeType;
+        },
+        addTextNodeIfNoChildren: function (frag) {
+            if (!frag.firstChild) {
+                frag.appendChild(frag.ownerDocument.createTextNode(''));
+            }
+        },
+        registerChildMutationCallback: function (tag, callback) {
+            if (callback) {
+                childMutationCallbacks[tag] = callback;
+            } else {
+                return childMutationCallbacks[tag];
+            }
+        },
+        callChildMutationCallback: function (el) {
+            var callback = el && childMutationCallbacks[el.nodeName.toLowerCase()];
+            if (callback) {
+                callback(el);
+            }
+        },
+        replace: function (nodes, val, teardown) {
+            var oldNodes = nodes.slice(0), frag = makeFrag(val);
+            nodeLists.register(nodes, teardown);
+            nodeLists.update(nodes, childNodes(frag));
+            nodeLists.replace(oldNodes, frag);
+            return nodes;
+        },
+        getParentNode: function (el, defaultParentNode) {
+            return defaultParentNode && el.parentNode.nodeType === 11 ? defaultParentNode : el.parentNode;
+        },
+        makeString: function (txt) {
+            return txt == null ? '' : '' + txt;
+        }
+    };
+    module.exports = live;
+});
---
+++ dist/cjs/lib/html.js
@@ -0,0 +1,37 @@
+/*can-view-live@3.2.4#lib/html*/
+var live = require('./core.js');
+var nodeLists = require('can-view-nodelist');
+var makeFrag = require('can-util/dom/frag/frag');
+var makeArray = require('can-util/js/make-array/make-array');
+var childNodes = require('can-util/dom/child-nodes/child-nodes');
+var canReflect = require('can-reflect');
+live.html = function (el, compute, parentNode, nodeList) {
+    var data, makeAndPut, nodes;
+    parentNode = live.getParentNode(el, parentNode);
+    data = live.listen(parentNode, compute, function (newVal) {
+        var attached = nodeLists.first(nodes).parentNode;
+        if (attached) {
+            makeAndPut(newVal);
+        }
+        var pn = nodeLists.first(nodes).parentNode;
+        data.teardownCheck(pn);
+        live.callChildMutationCallback(pn);
+    });
+    nodes = nodeList || [el];
+    makeAndPut = function (val) {
+        var isFunction = typeof val === 'function', frag = makeFrag(isFunction ? '' : val), oldNodes = makeArray(nodes);
+        live.addTextNodeIfNoChildren(frag);
+        oldNodes = nodeLists.update(nodes, childNodes(frag));
+        if (isFunction) {
+            val(frag.firstChild);
+        }
+        nodeLists.replace(oldNodes, frag);
+    };
+    data.nodeList = nodes;
+    if (!nodeList) {
+        nodeLists.register(nodes, data.teardownCheck);
+    } else {
+        nodeList.unregistered = data.teardownCheck;
+    }
+    makeAndPut(canReflect.getValue(compute));
+};
