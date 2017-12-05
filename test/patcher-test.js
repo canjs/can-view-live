bd89f95a2779038e2dd913272b448c200d92326f
James Atherton <james@bitovi.com>
2017-12-05 16:03:24 -0500

    Closes #76 - patcher won't error for undefined observables

--- test/patcher-test.js
+++ test/patcher-test.js
@@ -2,6 +2,7 @@ var DefineList = require("can-define/list/list");
 var QUnit = require('steal-qunit');
 var canSymbol = require("can-symbol");
 var Patcher = require("../lib/patcher");
+var SimpleObservable = require("can-simple-observable");
 
 QUnit.module("can-view-live patcher",{
 	setup: function(){
@@ -12,15 +13,26 @@ QUnit.module("can-view-live patcher",{
 
 QUnit.test('multiple lists can be updated at once', 2, function () {
 	var list = new DefineList(["a","b"]);
-    var p1 = new Patcher(list),
-        p2 = new Patcher(list);
+	var p1 = new Patcher(list),
+		p2 = new Patcher(list);
 
-    p1[canSymbol.for("can.onPatches")](function(){
-        QUnit.ok(true, "called p1");
-    });
-    p2[canSymbol.for("can.onPatches")](function(){
-        QUnit.ok(true, "called p2");
-    });
+	p1[canSymbol.for("can.onPatches")](function(){
+		QUnit.ok(true, "called p1");
+	});
+	p2[canSymbol.for("can.onPatches")](function(){
+		QUnit.ok(true, "called p2");
+	});
 
-    list.push("c");
+	list.push("c");
+});
+
+QUnit.test('undefined value won\'t error', 1, function () {
+	var undfinedObservable = new SimpleObservable(undefined);
+	var pu = new Patcher(undfinedObservable);
+
+	pu[canSymbol.for("can.onPatches")](function(){
+		QUnit.ok(true, "called pu");
+	});
+
+	undfinedObservable.set("a");
 });
